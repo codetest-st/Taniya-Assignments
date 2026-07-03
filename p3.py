19caf8e0078a65e872d0a8414d8e591710a5f10d
codetest-st <codetest.st@gmail.com>
2026-07-03 16:23:24 +0530

    program 3

---
+++ p3.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the 2nd number:"))
+a=num1/num2
+b=num1//num2
+print(f"The division of num1 and num2 is {a}")
+print(f"The division of num1 and num2 is {b}")
