b3f0a6a433c42448151beb43511c5018a4c22003
codetest-st <yash38daga@gmail.com>
2026-07-06 17:15:00 +0530

    Day 1 Assignment

---
+++ p17.py
@@ -0,0 +1,3 @@
+p=float(input("Enter the RD premium:"))
+maturity=(p/5)*340.27
+print(f"Maturity amount is {maturity}")
---
+++ p9.py
@@ -0,0 +1,3 @@
+r=int(input("Enter radius of circle:"))
+circumference=3.14*2*r
+print(f"The circumference of a circleis {circumference}")
